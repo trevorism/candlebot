50e9e0c07bc2bee6de5d893ffeba2648d71239b9
Trevor Brooks <trevorism@gmail.com>
2024-09-01 11:22:39 -0400

    Unit tests

--- src/test/groovy/com/trevorism/service/FileOrKrakenCandleProviderTest.groovy
+++ src/test/groovy/com/trevorism/service/FileOrKrakenCandleProviderTest.groovy
@@ -3,11 +3,13 @@ package com.trevorism.service
 import com.trevorism.kraken.model.Candle
 import org.junit.jupiter.api.Test
 
+import java.time.Duration
+
 class FileOrKrakenCandleProviderTest {
 
     @Test
     void testGetCandles() {
-        List<Candle> candleList = new FileOrKrakenCandleProvider().getCandles("BTCUSD")
+        List<Candle> candleList = new FileOrKrakenCandleProvider().getCandles("BTCUSD", Duration.ofDays(1))
         assert candleList.size() == 720
     }
 }
